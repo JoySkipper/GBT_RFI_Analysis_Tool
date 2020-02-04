81bd2667aaf9f625452c8562a2b60fd4da3c255a
JoySkipper <jns9md@virginia.edu>
2020-02-04 15:54:14 -0500

    added dictionary to change column names to correct ones

---
+++ rfitrends/Column_fixes.py
@@ -0,0 +1,20 @@
+"""
+.. module:: Column_fixes.py
+    :synopsis: streamlines multiple formats of GBT RFI data into one, consistent format
+.. moduleauthor:: Joy Skipper <jskipper@nrao.edu>
+Code Origin: https://github.com/JoySkipper/GBT_RFI_Analysis_Tool
+"""
+
+# Various column names we can get, streamlining to one format
+Column_name_corrections = {
+    "Frequency(MHz)": "Frequency_MHz",
+    "Frequency (MHz)": "Frequency_MHz",
+    "Frequency(GHz)": "Frequency_MHz",
+    "Frequency GHz)": "Frequency_MHz",
+    "Intensity(Jy)": "Intensity_Jy",
+    "Intenstiy (Jy)":"Intensity_Jy",
+    "Window": "Window",
+    "IFWindow":"Window",
+    "Channel":"Channel"
+}
+
