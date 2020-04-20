0b603185002947f1ee83515c6e72476171fe07d1
JoySkipper <jns9md@virginia.edu>
2020-04-20 17:01:38 -0400

    added tqdm as dependency

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = None
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'numpy', 'matplotlib', 'julian', 'mysql-connector-python',
+    'numpy', 'matplotlib', 'julian', 'mysql-connector-python','tqdm',
 ]
 
 # What packages are optional?
